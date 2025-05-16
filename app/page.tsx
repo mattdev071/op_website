3564fc42b1b9ace1a4acf093ef850ba4caf871f9
Never give up! <87398137+mattdev071@users.noreply.github.com>
2025-05-16 16:49:12 -0400

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -44,7 +44,7 @@ export default function Home() {
               <svg width="16" height="16" viewBox="0 0 24 24" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
                 <path d="M14 2H6c-1.1 0-2 .9-2 2v16c0 1.1.9 2 2 2h12c1.1 0 2-.9 2-2V8l-6-6zm-1 7V3.5L18.5 9H13z" fill="currentColor"/>
               </svg>
-              Whitepaper
+              YellowPaper
             </Link>
           </div>
         </div>
